b1c844dd095b77e077a6afda48d2f8e4cf5ac4af
Luis Mendez <hallo@luis-mendez.de>
2021-07-11 13:32:10 +0200

    adjusted gitignore. Get rid of assets folder

--- site/assets/active.php
+++
@@ -1,1 +0,0 @@
-<?php // Created by ProcessWire - Do not delete this file. The existence of this file indicates the site is confirmed active and first-time use errors may be suppressed. Installed at: [D:/laragon/www/symprowire/]
