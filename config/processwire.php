12c899567e7ec31f2ecb7cdc87ed3fe4341bd827
Luis Mendez <hallo@luis-mendez.de>
2021-07-12 18:39:48 +0200

    preparing RC1

---
+++ config/processwire.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Symfony\Component\DependencyInjection\Loader\Configurator;
+
+return static function (ContainerConfigurator $container) {
+
+    $root = $_SERVER['DOCUMENT_ROOT'];
+    $site = $root.'site/';
+
+    $container->parameters()->set('app.paths.root', $root);
+    $container->parameters()->set('app.paths.site', $site);
+    $container->parameters()->set('app.paths.modules', $site.'/modules/');
+    $container->parameters()->set('app.paths.assets', $site.'/assets/');
+    $container->parameters()->set('app.paths.templates', $site.'/templates/');
+
+};
