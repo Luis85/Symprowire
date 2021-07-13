e033d8e29dd4190192e482627b28ee42491dc57d
Luis Mendez <hallo@luis-mendez.de>
2021-07-13 13:25:02 +0200

    bump up Version to 020

--- Symprowire.module.php
+++ Symprowire.module.php
@@ -8,7 +8,7 @@ class Symprowire extends Wire implements Module {
         return [
             'title' => 'Symprowire - A Processwire Request/Response extension',
             'description' => 'A module integrating Symfony 5.3 with Twig to use Processwire in a MVC approach.',
-            'version' => 10,
+            'version' => 20,
             'summary' => 'Symprowire - Base Framework Module.',
             'href' => 'https://github.com/Luis85/symprowire',
             'singular' => true,
