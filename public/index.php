907b9becb3c122a3884ef59bde66b02a000be01a
Luis Mendez <hallo@luis-mendez.de>
2021-07-13 11:47:43 +0200

    refactored Session hand-shake to use ::replace() instead of foreach PW's session data.

--- public/index.php
+++ public/index.php
@@ -10,9 +10,9 @@ use function ProcessWire\wire;
 $session = new SymfonySession(new SessionBridge());
 $session->start();
 // add ProcessWire Session Data to our Sessionbag
-foreach(wire('session')->getAll() as $key => $value) {
-    $session->set($key, $value);
-}
+$session->replace(wire('session')->getAll());
+
+// instantiate the Kernel and let Symfony do his thing
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
