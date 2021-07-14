7697b561a6fb575d4a2ba6f31e4a8756278cdf60
Luis Mendez <hallo@luis-mendez.de>
2021-07-14 13:41:10 +0200

    added more context to HomeController and added example of ProcessWireLoggerService injection

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,25 +7,32 @@ use Symprowire\Controller\AbstractController;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symprowire\Repository\UserRepository;
+use Symprowire\Interfaces\ProcessWireLoggerServiceInterface;
+use Symprowire\Interfaces\UserRepositoryInterface;
 
 class HomeController extends AbstractController
 {
 
     /**
      * This Route does have a corresponding Page and therefore $this->page is populated accordingly
+     * We Inject the UserRepository and ProcessWireLoggerService for Demonstration
+     * As we are using Interfaces we use them instead of the Service for better test capabilities and CodeCompletition
      *
      * @Route("/", name="app_home")
      */
-    public function index(UserRepository $users): Response {
+    public function index(UserRepositoryInterface $users, ProcessWireLoggerServiceInterface $loggerService): Response {
 
+        // get the Homepage from the PagesRepository, available as Helper from AbstractController
         $title = $this->pages->getById(1)->title;
 
+        // log an Error for funsies
+        $loggerService->error('I am a test error log');
+
+        // Make some Vars available to the Frontend, $user and $session are automatically provided to Twig
         $vars = [
             'page' => $this->page,
             'pages' => $this->pages,
             'input' => $this->input,
-            'session' => $this->session,
             'modules' => $this->modules,
             'users' => $users,
         ];
@@ -38,7 +45,7 @@ class HomeController extends AbstractController
 
     /**
      * This Route does not have a corresponding Page
-     * $this->page will return the HomePage
+     * So keep in mind $this->page will be populated with $page->home
      *
      * @Route("/hello", name="app_hello")
      */
