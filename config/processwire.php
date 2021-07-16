67f1f1501acf0a87111874a5333cdbc0123a1574
Luis Mendez <hallo@luis-mendez.de>
2021-07-16 10:59:56 +0200

    fixed wrong path declaration in ProcessWire Container Config

--- config/processwire.php
+++ config/processwire.php
@@ -4,13 +4,13 @@ namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
 return static function (ContainerConfigurator $container) {
 
-    $root = $_SERVER['DOCUMENT_ROOT'];
-    $site = $root.'site/';
+    $root = $_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR;
+    $site = $root.'site'.DIRECTORY_SEPARATOR;
 
     $container->parameters()->set('app.paths.root', $root);
     $container->parameters()->set('app.paths.site', $site);
-    $container->parameters()->set('app.paths.modules', $site.'/modules/');
-    $container->parameters()->set('app.paths.assets', $site.'/assets/');
-    $container->parameters()->set('app.paths.templates', $site.'/templates/');
+    $container->parameters()->set('app.paths.modules', $site.'modules'.DIRECTORY_SEPARATOR);
+    $container->parameters()->set('app.paths.assets', $site.'assets'.DIRECTORY_SEPARATOR);
+    $container->parameters()->set('app.paths.templates', $site.'templates'.DIRECTORY_SEPARATOR);
 
 };
