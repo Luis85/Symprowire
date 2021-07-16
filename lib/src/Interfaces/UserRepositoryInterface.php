c76e56999c64a8abc58914460b15b390422252f3
Luis Mendez <hallo@luis-mendez.de>
2021-07-16 09:15:25 +0200

    replaced wire() function calls by implementing ProcessWire as Service.
    - added new Method getByName
    - added new toString Method

--- lib/src/Interfaces/UserRepositoryInterface.php
+++ lib/src/Interfaces/UserRepositoryInterface.php
@@ -10,7 +10,11 @@ interface UserRepositoryInterface
 {
     public function getByEmail(string $email): User;
 
+    public function getByName(string $name): User;
+
     public function getById(int $id): User;
 
     public function find(string $selector): PageArray;
+
+    public function __toString(): string;
 }
--- lib/src/Repository/UserRepository.php
+++ lib/src/Repository/UserRepository.php
@@ -3,21 +3,25 @@
 
 namespace Symprowire\Repository;
 
+use Symprowire\Interfaces\ProcessWireServiceInterface;
 use Symprowire\Interfaces\UserRepositoryInterface;
 use Processwire\User;
 use Processwire\PageArray;
-use ProcessWire\Wire;
 
-class UserRepository extends Wire implements UserRepositoryInterface
+class UserRepository implements UserRepositoryInterface
 {
     private $users;
 
-    public function __construct() {
-        $this->users = $this->wire('users');
+    public function __construct(ProcessWireServiceInterface $processWire) {
+        $this->users = $processWire->get('users');
     }
 
     public function getByEmail(string $email): User {
         return $this->users->get('email='.$email);
+    }
+
+    public function getByName(string $name): User {
+        return $this->users->get('name='.$name);
     }
 
     public function get(string $selector): User {
@@ -30,5 +34,9 @@ class UserRepository extends Wire implements UserRepositoryInterface
 
     public function find(string $selector): PageArray {
         return $this->users->findMany($selector);
+    }
+
+    public function __toString(): string {
+        return get_class($this);
     }
 }
