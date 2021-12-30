bc36d87d240600adf983979ba9be26a7b8f89ac5
Luis Mendez <hallo@luis-mendez.de>
2021-12-30 10:58:50 +0100

    - simplified controller.php

--- templates/controller.php
+++ templates/controller.php
@@ -1,34 +1,31 @@
 <?php namespace ProcessWire;
 
-use Exception;
 use Symprowire\Symprowire;
 
 
 /**
  * This is the Symprowire FrontController
+ * -----------------------------------------
  *
- * Every Exception thrown inside Symprowire should be handled by the Framework.
- * If the Framework itself fails, ProcessWire could catch up
+ * We require the composer autoloader
+ * Add some parameters to Symprowire
+ * Execute the App with the current ProcessWire instance
+ * if ProcessWire is running in debug and TracyDebugger is installed we will dump the whole executed Kernel to Tracy
+ * and echo a HTML string back to ProcessWire
  *
  */
 
 require_once($this->config->paths->site . 'vendor/autoload.php');
 
-try {
-    $symprowire = new Symprowire();
-    $symprowire->execute($this->wire);
-    if($this->modules->isInstalled('TracyDebugger') && $this->config->debug) {
-        bd($symprowire, 'Symprowire / Executed Kernel', [4]);
-    }
-    return $symprowire->render();
-}
-/**
- * We will catch every Exception thrown by Symprowire and serve a 404 if not in debug.
- * Error Handling is now served by ProcessWire again
- */
-catch (Exception $exception) {
-    $this->log->error($exception->getMessage());
-    if($this->config->debug) throw $exception;
-    throw new Wire404Exception();
+$params = [
+    'renderer' => 'twig',
+];
+
+$symprowire = new Symprowire($params);
+$symprowire->execute($this->wire);
+
+if($this->modules->isInstalled('TracyDebugger') && $this->config->debug) {
+    bd($symprowire, 'Symprowire / Executed Kernel', [4]);
 }
 
+echo $symprowire->render();
