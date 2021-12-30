fa72673f980be78cb7e485b1cdcf721520b58ea3
Luis Mendez <hallo@luis-mendez.de>
2021-12-30 13:37:10 +0100

    - added PW env dirs to the preloader

--- config/preload.php
+++ config/preload.php
@@ -3,3 +3,6 @@
 if (file_exists(dirname(__DIR__).'/var/cache/prod/Symprowire_KernelProdContainer.preload.php')) {
     require dirname(__DIR__).'/var/cache/prod/Symprowire_KernelProdContainer.preload.php';
 }
+if (file_exists(dirname(__DIR__).'/assets/symprowire/prod/cache/Symprowire_KernelProdContainer.preload.php')) {
+    require dirname(__DIR__).'/assets/symprowire/prod/cache/Symprowire_KernelProdContainer.preload.php';
+}
